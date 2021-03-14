ae4644e37c2d781a466d3d46e59a3f6366a3c174
Artsiom Murashko <ksarelto@tut.by>
2021-03-14 23:16:13 +0300

    feat: add solution for task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const allBracketsConfig = bracketsConfig.reduce((res, el) => res.concat(el.join('')), []);
+
+  function checkStr(str) {
+    let res = str;
+
+    for (let i = 0; i < allBracketsConfig.length; i++) {
+      if (str.includes(allBracketsConfig[i])) {
+        res = res.replace(allBracketsConfig[i], '');
+      }
+    }
+    if (res === str) {
+      return res;
+    } else {
+      return checkStr(res);
+    }
+  }
+
+  return checkStr(str) === '' ? true : false;
+
 }
+
